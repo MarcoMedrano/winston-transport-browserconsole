3b904443ddc25a03cd038b805705b39ebfc53c69
Marco Medrano <marco.medrano.garay@gmail.com>
2020-03-20 18:37:28 -0400

    fix(#3): changed module to commonjs, seems supporting both es5 and running jest(node)

--- dist/BrowserConsole.js
+++ dist/BrowserConsole.js
@@ -1,3 +1,4 @@
+"use strict";
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -11,70 +12,58 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "winston", "winston-transport"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var winston = require("winston");
-    // import * as TransportStream from 'winston-transport';
-    var TransportStream = require("winston-transport");
-    var BrowserConsole = /** @class */ (function (_super) {
-        __extends(BrowserConsole, _super);
-        function BrowserConsole(opts) {
-            var _this = _super.call(this, opts) || this;
-            _this.methods = {
-                debug: 'debug',
-                error: 'error',
-                info: 'info',
-                warn: 'warn',
-            };
-            // this.level = Level[Level.debug];
-            if (opts && opts.level && Level.hasOwnProperty(opts.level)) {
-                _this.level = opts.level;
-            }
-            else {
-                // TODO this is not getting the level configured in winston.configure
-                _this.level = winston.level;
-            }
-            return _this;
-        }
-        BrowserConsole.prototype.log = function (logEntry, next) {
-            var _this = this;
-            // (window as any).l = logEntry;
-            setImmediate(function () {
-                _this.emit('logged', logEntry);
-            });
-            var incommingLevel = Level[logEntry.level];
-            if (incommingLevel <= Level[this.level]) {
-                var message = logEntry.message, level = logEntry.level;
-                var mappedMethod = this.methods[level];
-                if (Object.getOwnPropertySymbols(logEntry).length === 2)
-                    console[mappedMethod](message);
-                else {
-                    // @ts-ignore
-                    var args = logEntry[Object.getOwnPropertySymbols(logEntry)[1]];
-                    args = args.length >= 1 ? args[0] : args;
-                    console[mappedMethod](message, args);
-                }
-            }
-            next();
+Object.defineProperty(exports, "__esModule", { value: true });
+var winston = require("winston");
+var TransportStream = require("winston-transport");
+var BrowserConsole = /** @class */ (function (_super) {
+    __extends(BrowserConsole, _super);
+    function BrowserConsole(opts) {
+        var _this = _super.call(this, opts) || this;
+        _this.methods = {
+            debug: 'debug',
+            error: 'error',
+            info: 'info',
+            warn: 'warn',
         };
-        return BrowserConsole;
-    }(TransportStream));
-    exports.default = BrowserConsole;
-    var Level;
-    (function (Level) {
-        Level[Level["error"] = 0] = "error";
-        Level[Level["warn"] = 1] = "warn";
-        Level[Level["info"] = 2] = "info";
-        Level[Level["debug"] = 4] = "debug";
-    })(Level || (Level = {}));
-});
+        // this.level = Level[Level.debug];
+        if (opts && opts.level && Level.hasOwnProperty(opts.level)) {
+            _this.level = opts.level;
+        }
+        else {
+            // TODO this is not getting the level configured in winston.configure
+            _this.level = winston.level;
+        }
+        return _this;
+    }
+    BrowserConsole.prototype.log = function (logEntry, next) {
+        var _this = this;
+        // (window as any).l = logEntry;
+        setImmediate(function () {
+            _this.emit('logged', logEntry);
+        });
+        var incommingLevel = Level[logEntry.level];
+        if (incommingLevel <= Level[this.level]) {
+            var message = logEntry.message, level = logEntry.level;
+            var mappedMethod = this.methods[level];
+            if (Object.getOwnPropertySymbols(logEntry).length === 2)
+                console[mappedMethod](message);
+            else {
+                // @ts-ignore
+                var args = logEntry[Object.getOwnPropertySymbols(logEntry)[1]];
+                args = args.length >= 1 ? args[0] : args;
+                console[mappedMethod](message, args);
+            }
+        }
+        next();
+    };
+    return BrowserConsole;
+}(TransportStream));
+exports.default = BrowserConsole;
+var Level;
+(function (Level) {
+    Level[Level["error"] = 0] = "error";
+    Level[Level["warn"] = 1] = "warn";
+    Level[Level["info"] = 2] = "info";
+    Level[Level["debug"] = 4] = "debug";
+})(Level || (Level = {}));
 //# sourceMappingURL=BrowserConsole.js.map
